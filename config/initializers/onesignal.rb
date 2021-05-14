44d71a4741650cfee326dd85d64aefdfd5fc21bd
Mihir Kumar Thakur <mihirthakur.cse@gmail.com>
2021-05-14 21:59:06 +0530

    FIxed onesignal settings

--- config/initializers/onesignal.rb
+++ config/initializers/onesignal.rb
@@ -3,6 +3,7 @@ require 'onesignal'
 OneSignal.configure do |config|
   config.app_id = ENV["ONE_SIGNAL_APP_ID"]
   config.api_key = ENV["ONE_SIGNAL_REST_API_KEY"]
-  config.active = false
-  config.logger = Logger.new # Any Logger compliant implementation
+  #config.api_url = "https://onesignal.com/api/v1"
+  config.active = true
+  #config.logger = Logger.new # Any Logger compliant implementation
 end
